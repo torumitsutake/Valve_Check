5144a682d2d44042c0c7fb4c396f660907e5da09
Toru Mitsutake <mitsumitsu.zikkyou@gmail.com>
2015-11-23 23:34:58 +0900

    first commit

---
+++ SerialChecker/LogForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialChecker
+{
+    public partial class LogForm : Form
+    {
+        public LogForm()
+        {
+            InitializeComponent();
+        }
+
+        public void changeBox(String str)
+        {
+            LogBox.AppendText(str);
+        }
+
+
+        public string getText()
+        {
+            return LogBox.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

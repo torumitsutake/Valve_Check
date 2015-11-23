dd2bb9ac019765ef5092a93f0d1900f892cf6668
Toru Mitsutake <mitsumitsu.zikkyou@gmail.com>
2015-11-24 00:02:07 +0900

    firstcommit

---
+++ SerialChecker/LoggerSample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialChecker
+{
+    public static class LoggerSample
+    {
+        
+
+        public static void Debug(string message)
+        {
+            DateTime dtNow = DateTime.Now;
+
+            StreamWriter wr = new StreamWriter(Form1.filepath,true, Encoding.GetEncoding("Shift_JIS"));
+            wr.WriteLine(dtNow.ToString() + "                                                     "+message);
+            wr.Close();
+
+        }
+
+       
+    }
+}
